e297866a1af5dc1ff81b1d3052fcdaa7bbbbc5ef
kanbara-ap <s.kanbara@alphapolis.co.jp>
2022-05-19 14:22:49 +0900

    Initial Commit

---
+++ Accounting app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Accounting app
+//
+//  Created by WEBSYSTEM-MAC31 on 2022/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
